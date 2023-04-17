88670ff937f7ebd5ba8f2ba6fb8076e25db746c3
Constantin Ginga <41089075+constantinginga@users.noreply.github.com>
2023-04-17 21:22:31 +0200

    CORS fix for front-end

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,14 @@
-import express from "express";
-import v1MovieRouter from "./v1/routes/movieRoutes"
-import bodyParser from "body-parser"
+import express from 'express';
+import v1MovieRouter from './v1/routes/movieRoutes';
+import cors from 'cors';
+import bodyParser from 'body-parser';
 
 const app = express();
 
+app.use(cors());
 app.use(bodyParser.json());
-app.use("/bestmovies/v1/movies", v1MovieRouter);
+app.use('/bestmovies/v1/movies', v1MovieRouter);
 
 const port = process.env.PORT || 3000;
-
 
 app.listen(port, () => console.log(`App listening on PORT ${port}`));
