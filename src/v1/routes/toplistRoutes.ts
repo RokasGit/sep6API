ac23a6b82752fd4c18b9e811e2871184f9e7df20
Constantin Ginga <41089075+constantinginga@users.noreply.github.com>
2023-04-24 21:12:44 +0200

    Change from GET to POST

--- src/v1/routes/toplistRoutes.ts
+++ src/v1/routes/toplistRoutes.ts
@@ -1,7 +1,7 @@
-import express, { Request, Response } from "express"
-import toplistController from "../controllers/toplistController"
+import express, { Request, Response } from 'express';
+import toplistController from '../controllers/toplistController';
 const router = express.Router();
 
-router.get("/:userId", toplistController.addMovieIdBasedOnUserId);
+router.post('/:userId', toplistController.addMovieIdBasedOnUserId);
 
 export = router;
