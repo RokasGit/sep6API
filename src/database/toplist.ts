b985b9844f0112b59c63c5711a3788245b9b36aa
Rokas Paulauskas <ifamous.rp@gmail.com>
2023-04-24 14:20:53 +0200

    Database fix

--- src/database/toplist.ts
+++ src/database/toplist.ts
@@ -1,13 +1,19 @@
-import knex from "knex";
+import db from "./index";
 
 export default class ToplistData {
-    static async addMovieIdToToplist(userId : number, movieId : number) : Promise<boolean> {
-        try{
-          const response = await knex("Toplist").insert({user_id: userId, imdb_movie_id: movieId});
-          return response.length > 0;
-        }catch(error){
-            console.log(error);
-            return false;
-        }
+  static async addMovieIdToToplist(
+    userId: number,
+    movieId: number
+  ): Promise<boolean> {
+    try {
+      const response = await db.db("Toplist").insert({
+        user_id: userId,
+        imdb_movie_id: movieId,
+      });
+      return response.length > 0;
+    } catch (error) {
+      console.log(error);
+      return false;
     }
+  }
 }
