364d8cf98dbbb500d077cc7f96cb5ca3f443f2f8
Rokas Paulauskas <ifamous.rp@gmail.com>
2023-05-18 15:22:13 +0200

    Try/catch for toplist/watchlist

--- src/v1/controllers/toplistController.ts
+++ src/v1/controllers/toplistController.ts
@@ -6,44 +6,52 @@ import { Movie } from "../../models/movie";
 
 export default class ToplistController {
   static async addMovieIdBasedOnUserId(req: Request, res: Response) {
-    const responseFromDB = await toplistService.addMovieIdBasedOnUser(
-      parseInt(req.params.userId),
-      req.body.imdbID
-    );
-    let responseBody;
-    if (responseFromDB) {
-      let body = req.body;
-      body.BelongsToToplist = true;
-      responseBody = body;
-    } else {
-      responseBody = undefined;
+    try {
+      const responseFromDB = await toplistService.addMovieIdBasedOnUser(
+        parseInt(req.params.userId),
+        req.body.imdbID
+      );
+      let responseBody;
+      if (responseFromDB) {
+        let body = req.body;
+        body.BelongsToToplist = true;
+        responseBody = body;
+      } else {
+        responseBody = undefined;
+      }
+      res.send({ status: "OK", data: responseBody });
+    } catch (error) {
+      res.status(400).json((error as Error).message);
     }
-    res.send({ status: "OK", data: responseBody });
   }
 
   static async getToplistBasedOnUserId(req: Request, res: Response) {
-    const responseFromDB = await toplistService.getToplistBasedOnUserId(
-      parseInt(req.params.userId)
-    );
-    let responseBody: string[] = [];
-    const promises: any[] = [];
-    responseFromDB.forEach((movieId) => {
-      promises.push(
-        movieService.getOneMovieById(Object.entries(movieId)[0][1])
+    try {
+      const responseFromDB = await toplistService.getToplistBasedOnUserId(
+        parseInt(req.params.userId)
       );
-    });
-    responseBody = await Promise.all(promises);
-    const modifiedObjects = await Promise.all(
-      responseBody.map(async (movie) => {
-        const JSONobject = JSON.parse(JSON.stringify(movie));
-        JSONobject.BelongsToWatchlist = await WatchlistService.isInWatchlist(
-          parseInt(req.params.userId),
-          JSONobject.imdbID
+      let responseBody: string[] = [];
+      const promises: any[] = [];
+      responseFromDB.forEach((movieId) => {
+        promises.push(
+          movieService.getOneMovieById(Object.entries(movieId)[0][1])
         );
-        return JSONobject;
-      })
-    );
-    res.send({ status: "OK", data: modifiedObjects });
+      });
+      responseBody = await Promise.all(promises);
+      const modifiedObjects = await Promise.all(
+        responseBody.map(async (movie) => {
+          const JSONobject = JSON.parse(JSON.stringify(movie));
+          JSONobject.BelongsToWatchlist = await WatchlistService.isInWatchlist(
+            parseInt(req.params.userId),
+            JSONobject.imdbID
+          );
+          return JSONobject;
+        })
+      );
+      res.send({ status: "OK", data: modifiedObjects });
+    } catch (error) {
+      res.status(400).json((error as Error).message);
+    }
   }
 
   // static async getToplistBasedOnUserId(req: Request, res: Response) {
@@ -62,18 +70,22 @@ export default class ToplistController {
   // }
 
   static async deleteMovieFromToplist(req: Request, res: Response) {
-    const responseFromDB = await toplistService.deleteMovieFromToplist(
-      parseInt(req.params.userId),
-      req.body.imdbID
-    );
-    let responseBody: string[] = [];
-    const promises: any[] = [];
-    responseFromDB.forEach((movieId) => {
-      promises.push(
-        movieService.getOneMovieById(Object.entries(movieId)[0][1])
+    try {
+      const responseFromDB = await toplistService.deleteMovieFromToplist(
+        parseInt(req.params.userId),
+        req.body.imdbID
       );
-    });
-    responseBody = await Promise.all(promises);
-    res.status(200).json(responseBody);
+      let responseBody: string[] = [];
+      const promises: any[] = [];
+      responseFromDB.forEach((movieId) => {
+        promises.push(
+          movieService.getOneMovieById(Object.entries(movieId)[0][1])
+        );
+      });
+      responseBody = await Promise.all(promises);
+      res.status(200).json(responseBody);
+    } catch (error) {
+      res.status(400).json((error as Error).message);
+    }
   }
 }
--- src/v1/controllers/watchlistController.ts
+++ src/v1/controllers/watchlistController.ts
@@ -5,59 +5,71 @@ import ToplistService from "../services/toplistService";
 
 export default class WatchlistController {
   static async addMovieIdBasedOnUserId(req: Request, res: Response) {
-    const responseFromDB = await watchlistService.addMovieIdBasedOnUser(
-      parseInt(req.params.userId),
-      req.body.imdbID
-    );
-    let responseBody;
-    if (responseFromDB) {
-      let body = req.body;
-      body.BelongsToWatchlist = true;
-      responseBody = body;
-    } else {
-      responseBody = undefined;
+    try {
+      const responseFromDB = await watchlistService.addMovieIdBasedOnUser(
+        parseInt(req.params.userId),
+        req.body.imdbID
+      );
+      let responseBody;
+      if (responseFromDB) {
+        let body = req.body;
+        body.BelongsToWatchlist = true;
+        responseBody = body;
+      } else {
+        responseBody = undefined;
+      }
+      res.send({ status: "OK", data: responseBody });
+    } catch (error) {
+      res.status(400).json((error as Error).message);
     }
-    res.send({ status: "OK", data: responseBody });
   }
 
   static async getWatchlistBasedOnUserId(req: Request, res: Response) {
-    const responseFromDB = await watchlistService.getWatchlistBasedOnUserId(
-      parseInt(req.params.userId)
-    );
-    let responseBody: String[] = [];
-    const promises: any[] = [];
-    responseFromDB.forEach((movieId) => {
-      promises.push(
-        movieService.getOneMovieById(Object.entries(movieId)[0][1])
+    try {
+      const responseFromDB = await watchlistService.getWatchlistBasedOnUserId(
+        parseInt(req.params.userId)
       );
-    });
-    responseBody = await Promise.all(promises);
-    const modifiedObjects = await Promise.all(
-      responseBody.map(async (movie) => {
-        const JSONobject = JSON.parse(JSON.stringify(movie));
-        JSONobject.BelongsToToplist = await ToplistService.isInToplist(
-          parseInt(req.params.userId),
-          JSONobject.imdbID
+      let responseBody: String[] = [];
+      const promises: any[] = [];
+      responseFromDB.forEach((movieId) => {
+        promises.push(
+          movieService.getOneMovieById(Object.entries(movieId)[0][1])
         );
-        return JSONobject;
-      })
-    );
-    res.send({ status: "OK", data: modifiedObjects });
+      });
+      responseBody = await Promise.all(promises);
+      const modifiedObjects = await Promise.all(
+        responseBody.map(async (movie) => {
+          const JSONobject = JSON.parse(JSON.stringify(movie));
+          JSONobject.BelongsToToplist = await ToplistService.isInToplist(
+            parseInt(req.params.userId),
+            JSONobject.imdbID
+          );
+          return JSONobject;
+        })
+      );
+      res.send({ status: "OK", data: modifiedObjects });
+    } catch (error) {
+      res.status(400).json((error as Error).message);
+    }
   }
 
   static async deleteMovieFromWatchlist(req: Request, res: Response) {
-    const responseFromDB = await watchlistService.deleteMovieFromWatchlist(
-      parseInt(req.params.userId),
-      req.body.imdbID
-    );
-    let responseBody: String[] = [];
-    const promises: any[] = [];
-    responseFromDB.forEach((movieId) => {
-      promises.push(
-        movieService.getOneMovieById(Object.entries(movieId)[0][1])
+    try {
+      const responseFromDB = await watchlistService.deleteMovieFromWatchlist(
+        parseInt(req.params.userId),
+        req.body.imdbID
       );
-    });
-    responseBody = await Promise.all(promises);
-    res.status(200).json(responseBody);
+      let responseBody: String[] = [];
+      const promises: any[] = [];
+      responseFromDB.forEach((movieId) => {
+        promises.push(
+          movieService.getOneMovieById(Object.entries(movieId)[0][1])
+        );
+      });
+      responseBody = await Promise.all(promises);
+      res.status(200).json(responseBody);
+    } catch (error) {
+      res.status(400).json((error as Error).message);
+    }
   }
 }
