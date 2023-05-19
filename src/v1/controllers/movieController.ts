241e540bcf5edbcbb211a8cbbb34f3d9480b86d7
Mansory888 <nichita.railean@aol.com>
2023-05-19 10:50:54 +0200

    movie contrl

--- src/v1/controllers/movieController.ts
+++ src/v1/controllers/movieController.ts
@@ -7,21 +7,62 @@ import WatchlistService from "../services/watchlistService";
 
 export default class MovieController {
   static async getMoviesByTitle(req: Request, res: Response): Promise<void> {
-    const movies = await movieService.getMoviesByTitle(req.params.movieTitle);
-    const userID = parseFloat(req.params.userID);
-    const modifiedObjects = await Promise.all(movies.Search.map(async(movie: Movie) => {
-      movie.BelongsToToplist = await ToplistService.isInToplist(userID, movie.imdbID);
-      return movie;
-    }));
-    res.status(200).json(modifiedObjects);
+    try {
+      const movies = await movieService.getMoviesByTitle(req.params.movieTitle);
+      const userID = parseFloat(req.params.userID);
+      const review = await ReviewService.isReviewed(userID, movie.imdbID);
+
+      const modifiedObjects = await Promise.all(
+        movies.Search.map(async (movie: Movie) => {
+          movie.BelongsToToplist = await ToplistService.isInToplist(
+            userID,
+            movie.imdbID
+          );
+          movie.BelongsToWatchlist = await WatchlistService.isInWatchlist(
+            userID,
+            movie.imdbID
+          );
+
+          if (review) {
+            movie.review = review;
+          }
+
+          return movie;
+        })
+      );
+      res.status(200).json(modifiedObjects);
+    } catch (error) {
+      console.error(error);
+      res
+        .status(500)
+        .json({ message: "An error occurred while searching for the movie." });
+    }
   }
 
-  static async getOneMovieByTitle(req: Request, res: Response): Promise<void> {
+  static async getOneMovieById(req: Request, res: Response): Promise<void> {
     try {
-      const movie = await movieService.getOneMovieByTitle(
-        req.params.movieTitle
+      const movie = JSON.parse(
+        JSON.stringify(await movieService.getOneMovieById(req.params.imdbID))
       );
-      res.status(200).json(movie);
+      const userID = parseFloat(req.params.userID);
+      const review = await ReviewService.isReviewed(userID, movie.imdbID);
+
+      const responseBody = new Promise(async () => {
+        movie.BelongsToToplist = await ToplistService.isInToplist(
+          userID,
+          movie.imdbID
+        );
+        movie.BelongsToWatchlist = await WatchlistService.isInWatchlist(
+          userID,
+          movie.imdbID
+        );
+
+        if (review) {
+          movie.review = review;
+        }
+
+        res.status(200).json(movie);
+      });
     } catch (error) {
       console.error(error);
       res
